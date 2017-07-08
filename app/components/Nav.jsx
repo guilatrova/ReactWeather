5baeff5d611249813f78b2b75c908183cd1cb4c6
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-07-08 17:27:56 -0300

    redesign nav component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Link, IndexLink} from 'react-router';
 
-var Nav = React.createClass({
-    render: function() {
+class Nav extends React.Component {
+
+    onSearch(e) {
+        e.preventDefault();
+        alert('Not yet wired up!');        
+    }
+
+    render () {
         const active = {
             activeClassName: 'active',
             activeStyle: {
@@ -11,14 +17,32 @@ var Nav = React.createClass({
         }
 
         return (
-            <div>
-                <h2>NAV</h2>
-                <IndexLink to="/" {...active}>Get Weather</IndexLink>
-                <Link to="/about" {...active}>About</Link>
-                <Link to="/examples" {...active}>Examples</Link>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            <IndexLink to="/" {...active}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" {...active}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" {...active}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="Search weather" /></li>
+                            <li><input type="submit" className="button" value="Get Weather" /></li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         );
     }
-});
+};
 
 module.exports = Nav;
